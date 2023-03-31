2d1ac687139657bb6b932895f421804d0fbf29ff
nardien <nardeenashraf2014@gmail.com>
2023-03-31 14:33:31 +0200

    adding AccountPage routing

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import Navbar from "./Components/Navbar/Navbar";
 import Home from "./Pages/Home/Home";
 import Footer from "./Components/Footer/Footer";
 import { Slider } from "react-slick";
+import { Router, Routes } from "react-router-dom";
+import Account from "./Components/AccountPage/Account";
 
 
 
@@ -10,6 +12,9 @@ function App() {
     <div className="App">
       <Navbar />
       <Home />
+      <Routes>
+        <Router path="/account" element={<Account/>}/>
+      </Routes>
       <Footer />
     </div>
   );
